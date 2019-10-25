a638fd12a9a5af1e091cb6498d74a419bd7ad68d
ninpeng <ninpeng@naver.com>
2019-10-25 22:56:02 +0900

    import unfetch polyfill

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ApolloClient from 'apollo-boost';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from '@apollo/react-hooks';
 import fetch from 'unfetch';
+import 'unfetch/polyfill';
 
 import DefaultLayout from './containers/DefaultLayout';
 
