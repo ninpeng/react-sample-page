ecf5713b188fbfeafd0542dec82fbf22c5fec5fb
ninpeng <ninpeng@naver.com>
2019-10-25 22:48:25 +0900

    add unfetch polyfill

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ApolloClient from 'apollo-boost';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from '@apollo/react-hooks';
+import fetch from 'unfetch';
 
 import DefaultLayout from './containers/DefaultLayout';
 
 const client = new ApolloClient({
   uri: 'https://ninpeng-movie.herokuapp.com',
   cache: new InMemoryCache(),
+  fetchOptions: { fetch },
 });
 
 function App() {
